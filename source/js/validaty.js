07c2eaf5c1a0d73a242fd1f40aa18917218eaac0
Мария <mane44ka86@mail.ru>
2019-05-03 07:11:37 +0500

    Правки по валидации

--- source/js/validaty.js
+++ source/js/validaty.js
@@ -1,11 +1,17 @@
 var button = document.querySelector(".form__button");
 var input = document.querySelectorAll("input[required]");
+var icon = document.querySelectorAll(".input__icon")
 
 button.addEventListener("click", function () {
   for (var i = 0; i < input.length; i++) {
     if (!input[i].value) {
-      input[i].classList.add("input__error");
-      alert("Заполните обязательные поля")
+      input[i].classList.add("input--error");
+    }
+  }
+
+  for (var j = 0; j < icon.length; j++) {
+    if (!input[j].value) {
+      icon[j].classList.add("input__icon--error");
     }
   }
 });
